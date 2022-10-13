b97a78e120927a6d0282656857ecc58e41dbd289
forrestmorgan <96036883+forresttheman@users.noreply.github.com>
2022-10-13 20:18:38 +0000

    ^ display method, console -> screen

--- main.py
+++ main.py
@@ -6,30 +6,34 @@ import random
 pygame.init()
 
 #screen size variables
-w_mult = 80
-h_mult = 80
-
-screen_width = 10
-screen_height = 1
-
-# set the screen
-screen = pygame.display.set_mode((screen_width * w_mult, screen_height * h_mult))
-pygame.display.set_caption('StarryFactory')
-
-
-# rules for pixel interpretation
-neighborsToLive = 1
-neighborsToDie = 2
+screen_width = 300
+screen_height = 300
 
 # the amount of distance between the pixels
-screenStepX = 10
-screenStepY = 10
+screenStepX = 16
+screenStepY = 16
+
+# the distance the entire 
+# pixel zone will be shifted
+offsetX = 10
+offsetY = 10
+
+# set the screen
+screen = pygame.display.set_mode(
+    (screen_width, screen_height))
+pygame.display.set_caption('StarryFactory')
+
+# rules for pixel interpretation
+neighborsToLive = 2
+neighborsToDie = 4
+
+
 
 # list that pixels are stored in
 pixelsList = []
 
 # pixel images
-pixelImg = pygame.image.load('pixelImg.png')
+pixelImg = pygame.image.load('1.png')
 
 ################
 # PIXELS STUFF #
@@ -41,96 +45,142 @@ def Run():
     InterpretPixels(pixelsList)
     BlitPixels()
 
+def SetScreenSpacing():
+  pass
 
 def Set_Pixel_Array():
-  print("pixels SET")
-  # list that pixels are stored in
-  pixelsList = [[random.randint(0, 1) for x in range (screen_width)] for y in range (screen_height)]
-  return pixelsList
+    # list that pixels are stored in
+    pixelsList = [[random.randint(0, 1) for x in  range(screen_width)]
+                  for y in range(screen_height)]
+    return pixelsList
 
 
-def BlitPixels(list):
-  print("BLIT")
-  screen.fill((0, 0, 0))
-  for y in range (screen_height):
-    for x in range (screen_width):
-      if list[y][x] == 1:
-        screen.blit(pixelImg, (x * screenStepX, y * screenStepY))
+def BlitPixels():
+    screen.fill((255, 255, 255))
+    for y in range(screen_height):
+        for x in range(screen_width):
+            if pixelsList[y][x] == 1:
+                screen.blit(pixelImg, ((x * screenStepX) + offsetX, (y * screenStepY) + offsetY))
 
-######
-######
+
+              
+########################
+# PIXEL INTERPRETATION #
+########################
 
 def InterpretPixels(list):
-  # iterate through pixels
-  time.sleep(1)
-  for y in range (screen_height):
-    for x in range (screen_width):
-      
-      # Check neighbor pixel states (1 or 0?)
-      neighbors = 0
-  
-      # First neighbor: index +1
-      unNormalizedIndex = i + 1
-      if (unNormalizedIndex > len(list) - 1):
-        normalizedIndex = 0
-      else:
-        normalizedIndex = i + 1
-  
-      if list[normalizedIndex] == 1:
-        neighbors += 1
-  
-      # Second neighbor: index - 1
-      #make sure that that index exists!
-      if (list[i - 1] != None):
-        if list[i - 1] == 1:
-          neighbors += 1
-  
-      # Third neighbor: index + screen width
-      #check if that index exists!
-      unNormalizedIndex = i + screen_width
-  
-      if unNormalizedIndex > len(list):
-        normalizedIndex = 1
-      else:
-        normalizedIndex = i
-  
-      if list[normalizedIndex] == 1:
-        neighbors += 1
-  
-      # Fourth neighbor: index - screen width
-      unNormalizedIndex = i - screen_width
-  
-      if unNormalizedIndex < len(list):
-        normalizedIndex = 0
-      else:
-        normalizedIndex = i
-  
-      if list[normalizedIndex] == 1:
-        neighbors += 1
-  
-      # Toggle pixel state based on neighbors
-      pixel = list[i]
-  
-      # Loneliness....
-      if neighbors < neighborsToLive:
-        pixel = 0
-  
-      # Good amount of neighbors!
-      if neighbors >= neighborsToLive & neighbors < neighborsToDie:
-        pixel = 1
-  
-      # Overcrowding
-      if neighbors >= neighborsToDie:
-        pixel = 0
-  
-      # change pixel's value in list
-      list[i] = pixel
+    # iterate through pixels
+    time.sleep(1)
+    for y in range(screen_height):
+        for x in range(screen_width):
+          
+            # Check neighbor pixel states (1 or 0?)
+            neighbors = 0
+
+            ###########################
+            #   NEIGHBOR BLOCK 1      #
+            ###########################
+              # x index +1
+
+            # does this element even exist here?
+            if (x + 1 > len(list[y]) -1):
+                hasNeighbor = False
+                #  nope!
+            else:
+                hasNeighbor = True
+                # it does!
+
+            if hasNeighbor:
+              if list[y][x + 1] == 1:
+                neighbors += 1
+
+                
+            ###########################
+            #   NEIGHBOR BLOCK 2      #
+            ###########################
+              # x index - 1
+                
+
+            # does this element even exist here?
+            if (list[y][x - 1] == None):
+                hasNeighbor = False
+                #  nope!
+            else:
+                hasNeighbor = True
+                # it does!
+
+            if hasNeighbor:
+              if list[y][x - 1] == 1:
+                neighbors += 1
+
+                  
+            ###########################
+            #   NEIGHBOR BLOCK 3      #
+            ###########################    
+              # y index - 1
+
+            # does this element even exist here?
+            if (list[y - 1][x] == None):
+                hasNeighbor = False
+                #  nope!
+            else:
+                hasNeighbor = True
+                # it does!
+
+            if hasNeighbor:
+               neighbors += 1
+
+              
+            ###########################
+            #   NEIGHBOR BLOCK 4      #
+            ###########################
+              # y index + 1
+                
+            #does this element even exist here?
+            if (y + 1 > len(list) - 1):
+                hasNeighbor = False
+                #  nope!
+            else:
+                hasNeighbor = True
+                # it does!
+
+            if hasNeighbor:
+              if list[y + 1][x] == 1:
+                neighbors += 1
+
+
+              
+            ###########################
+            #   MANAGE PIXEL STATES   #
+            ###########################
+              
+            # Toggle pixel state based on neighbors
+            pixel = list[y][x]
+
+            # Loneliness....
+            if neighbors < neighborsToLive:
+                pixel = 0
+
+            # Good amount of neighbors!
+            if neighbors >= neighborsToLive & neighbors < neighborsToDie:
+                pixel = 1
+
+            # Overcrowding
+            if neighbors >= neighborsToDie:
+                pixel = 0
+
+            # change pixel's value in list
+            list[y][x] = pixel
+
 
 ########
 # LOOP #
 ########
+
 pixelsList = Set_Pixel_Array()
+
 while True:
+    screen.fill((0, 0, 255))
     # update the pixels
     Run()
 
@@ -139,7 +189,5 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-          
-    pygame.display.update()
 
-  
+    pygame.display.update()
